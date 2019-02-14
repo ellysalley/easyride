cb7c06fdc6472277a769197b373db0fa14052214
Elly <ellyhahn@gmail.com>
2019-02-13 22:11:14 -0800

    Add relationships

--- src/entities/Ride.ts
+++ src/entities/Ride.ts
@@ -3,10 +3,12 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
-  UpdateDateColumn
+  UpdateDateColumn,
 } from "typeorm";
 import {rideStatus} from "../types/types";
+import User from "./User";
 
 @Entity()
 class Ride extends BaseEntity {
@@ -17,25 +19,39 @@ class Ride extends BaseEntity {
     enum: ["ACCEPTED", "FINISHED", "CANCELED", "REQUESTING", "ONROUTE"]
   })
   status: rideStatus;
-
+  
   @Column({ type: "text"})
   pickUpAddress: string;
+  
   @Column({ type: "double precision", default: 0})
   pickUpLat: number;
+  
   @Column({ type: "double precision", default: 0})
   pickUpLng: number;
+  
   @Column({ type: "text"})
   dropOffAddress: string;
+  
   @Column({ type: "double precision", default: 0})
   dropOffLat: number;
+  
   @Column({ type: "double precision", default: 0})
   dropOffLng: number;
+  
   @Column({ type: "double precision", default: 0})
   price: number
+  
   @Column({ type: "text"})
   distance: string;
+  
   @Column({ type: "text"})
   duration: string;
+  
+  @ManyToOne(type => User, user => user.ridesAsPassenger)
+  passenger: User;
+
+  @ManyToOne(type => User, user => user.ridesAsDriver)
+  driver: User;  
 
   @CreateDateColumn() createAt: string;
   
