f99a9a9fc50048d56c326c40e7444ffc74a81925
Elly <ellyhahn@gmail.com>
2019-02-25 15:36:09 -0800

    Add client

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import { ApolloProvider } from "react-apollo";
+import React from "react";
+import ReactDOM from "react-dom";
+import client from "./apollo";
+import App from "./App";
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
